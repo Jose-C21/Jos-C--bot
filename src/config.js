4f8b8a0d4e58adb641b74aa994b06dd90643e144
Jose C <168625477+Jose-C21@users.noreply.github.com>
2026-02-01 21:12:31 -0600

    Actualizar config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
   prefix: ".",
-  owners: ["50432213256", "18057074359"],
+  owners: ["50432213256", "18057074359"],      // números reales
+  ownersLid: ["208272208490541"],               // números LID
   sessionsDir: "sessions",
   allowlistFile: "data/allowlist.json"
 }
