a38755c719ce30a0ce8f3d73c49ebefc17220445
Jose C <168625477+Jose-C21@users.noreply.github.com>
2026-02-04 15:03:27 -0600

    Actualizar textsticker.js

--- src/commands/textsticker.js
+++ src/commands/textsticker.js
@@ -68,7 +68,7 @@ async function buildFrames(text) {
     "#00c7ff", "#0a84ff", "#bf5af2", "#ff375f"
   ]
 
-  const fps = 2
+  const fps = 3
   const totalFrames = 16
   const fontFamily = "sans-serif"
 
