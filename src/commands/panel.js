4a58d517d6466c94409ffed8c05f48e0107ac9d2
Jose C <168625477+Jose-C21@users.noreply.github.com>
2026-03-07 20:03:21 -0600

    Actualizar panel.js

--- src/commands/panel.js
+++ src/commands/panel.js
@@ -1,18 +1,45 @@
-import { generateWAMessageFromContent, proto } from "baileys"
+import { generateWAMessageContent, generateWAMessageFromContent, proto } from "baileys"
 
 export default async function panel(sock, msg){
 
 const chat = msg.key.remoteJid
 
+// imagen del producto
+const image = await generateWAMessageContent({
+image:{
+url:"https://i.postimg.cc/TwGh4vDP/IMG-1651.png"
+}
+},{upload:sock.waUploadToServer})
+
 const message = generateWAMessageFromContent(chat,
+
 proto.Message.fromObject({
 
 viewOnceMessage:{
 message:{
 interactiveMessage:{
 
+header:{
+title:"🌴 PANEL DEL BOT",
+hasMediaAttachment:true,
+
+productMessage:{
+product:{
+productImage:image.imageMessage,
+productId:"26269801872707544",
+title:"Panel del Bot",
+description:"Sistema interactivo",
+retailerId:"panel-bot",
+url:"https://wa.me/p/26269801872707544/50433307811",
+productImageCount:1
+},
+businessOwnerJid:"50433307811@s.whatsapp.net"
+}
+
+},
+
 body:{
-text:"🌴 PANEL DEL BOT\nSelecciona una opción"
+text:"Selecciona una opción"
 },
 
 footer:{
@@ -53,8 +80,10 @@ id:".config"
 }
 }
 
-}),{})
+})
 
-await sock.relayMessage(chat, message.message, { messageId: message.key.id })
+,{}) 
+
+await sock.relayMessage(chat,message.message,{messageId:message.key.id})
 
 }
