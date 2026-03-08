6fee4f52a0627b852014b1a4708d102190155e0e
Jose C <168625477+Jose-C21@users.noreply.github.com>
2026-03-07 19:20:56 -0600

    Actualizar panel.js

--- src/commands/panel.js
+++ src/commands/panel.js
@@ -1,25 +1,31 @@
-await sock.sendMessage(chatId, {
-text: "🌴 PANEL DEL BOT\nSelecciona una opción",
-footer: "Sistema del Bot",
-title: "Menú",
-buttonText: "Abrir menú",
-sections: [
+export default async function panel(sock, msg) {
+
+const chatId = msg.key.remoteJid
+
+await sock.sendMessage(chatId,{
+text:"🌴 PANEL DEL BOT\nSelecciona una opción",
+footer:"Sistema del Bot",
+title:"Menú",
+buttonText:"Abrir menú",
+sections:[
 {
-title: "Opciones",
-rows: [
+title:"Opciones",
+rows:[
 {
-title: "🎧 Descargas",
-rowId: ".play"
+title:"🎧 Descargas",
+rowId:".play"
 },
 {
-title: "🎮 Juegos",
-rowId: ".juegos"
+title:"🎮 Juegos",
+rowId:".juegos"
 },
 {
-title: "⚙️ Configuración",
-rowId: ".config"
+title:"⚙️ Configuración",
+rowId:".config"
 }
 ]
 }
 ]
 })
+
+}
