dd493a2a26aceee9e57a2959e010bc8527ec548d
Jose C <168625477+Jose-C21@users.noreply.github.com>
2026-03-23 22:09:52 -0600

    Actualizar app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,25 @@
 import { startSock } from "./adapter/baileys.js"
 import { routeMessage } from "./core/router.js"
+import { exec } from "child_process"
 
 await startSock(routeMessage)
+
+
+setInterval(() => {
+  exec("git pull", (err, stdout) => {
+    if (err) return
+
+    if (
+      stdout.includes("Updating") ||
+      stdout.includes("changed") ||
+      stdout.includes("Fast-forward")
+    ) {
+      console.log(`
+🔄 ACTUALIZACIÓN DESDE GITHUB
+${stdout}
+🚀 REINICIANDO BOT...
+`)
+      process.exit(0)
+    }
+  })
+}, 15000)
