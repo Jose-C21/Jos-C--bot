0b456f9dbbc354d82ea2ea0687b6caa325eea6f4
Jose C <168625477+Jose-C21@users.noreply.github.com>
2026-03-23 22:34:15 -0600

    Actualizar app.js

--- src/app.js
+++ src/app.js
@@ -1,47 +1,4 @@
 import { startSock } from "./adapter/baileys.js"
-import { exec } from "child_process"
+import { routeMessage } from "./core/router.js"
 
-let currentRoute
-
-
-const loadRouter = async () => {
-  const module = await import(`./core/router.js?update=${Date.now()}`)
-  currentRoute = module.routeMessage
-}
-
-
-const dynamicRoute = (...args) => {
-  return currentRoute(...args)
-}
-
-
-await loadRouter()
-
-
-await startSock(dynamicRoute)
-
-
-setInterval(() => {
-  exec("git pull", async (err, stdout) => {
-    if (err) return
-
-    if (
-      stdout.includes("Updating") ||
-      stdout.includes("changed") ||
-      stdout.includes("Fast-forward")
-    ) {
-      console.log(`
-🔄 ACTUALIZACIÓN DESDE GITHUB
-${stdout}
-♻️ RECARGANDO ROUTER REAL...
-`)
-
-      try {
-        await loadRouter()
-        console.log("✅ Router actualizado sin reiniciar")
-      } catch (e) {
-        console.error("❌ Error recargando:", e)
-      }
-    }
-  })
-}, 15000)
+await startSock(routeMessage)
