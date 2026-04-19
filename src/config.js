c736fb3e86962af43b16312c5c1b96b28bd3943d
Jose C <168625477+Jose-C21@users.noreply.github.com>
2026-04-19 10:51:26 -0600

    Update owners and ownersLid in config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   prefix: ".",
-  owners: ["50433307811", "18057074359"],      // números reales
-  ownersLid: ["74801888637137", "19580839829625"],               // números LID
+  owners: ["50433543542", "18057074359"],      // números reales
+  ownersLid: ["111651969888394", "19580839829625"],               // números LID
   sessionsDir: "sessions",
   allowlistFile: "data/allowlist.json"
 }
