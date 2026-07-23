7df9f6775e50178f0ba7823d43a967198a275d3f
Jose C <168625477+Jose-C21@users.noreply.github.com>
2026-07-23 02:20:14 -0600

    Actualizar audiodoc.js

--- src/commands/audiodoc.js
+++ src/commands/audiodoc.js
@@ -4,7 +4,7 @@ import { getSenderJid, jidToNumber } from "../utils/jid.js"
 
 function signature() {
   // ✅ firma nueva
-  return `⟣ ©️ 𝓬𝓸𝓹𝔂𝓻𝓲𝓰𝓱𝓽|частная система\n> ⟣ 𝗢𝘄𝗻𝗲𝗿: 𝐽𝑜𝑠𝑒 𝐶 - 𝐾𝑎𝑡ℎ𝑦`
+  return `⟣ ©️ 𝓬𝓸𝓹𝔂𝓻𝓲𝓰𝓱𝓽|𝘑𝘊𝘒𝘡 𝘐𝘯𝘧𝘪𝘯𝘪𝘵𝘺 𝘓𝘰𝘰𝘱\n> ⟣ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿𝘀 & 𝗗𝗲𝘃: 𝐽𝑜𝑠𝑒 𝐶 - 𝐾𝑎𝑡ℎ𝑦`
 }
 
 export default async function audiodoc(sock, msg) {
