437bdc3e802293de9eb414808645bb98f0f29d15
Jose C <168625477+Jose-C21@users.noreply.github.com>
2026-07-23 23:03:23 -0600

    Actualizar perfil.js

--- src/commands/perfil.js
+++ src/commands/perfil.js
@@ -19,7 +19,7 @@ const ADMINS_CONFIANZA = [
 // ✅ Firma que ya usas
 const FIRMA =
 `⟣ ©️ 𝓬𝓸𝓹𝔂𝓻𝓲𝓰𝓱𝓽|𝘑𝘊𝘒𝘡𝘐𝘯𝘧𝘪𝘯𝘪𝘵𝘺𝘓𝘰𝘰𝘱
-> ⟣ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿𝘀 & 𝗗𝗲𝘃: 𝐽𝑜𝑠𝑒 𝐶 - 𝐾𝑎𝑡ℎ𝑦`
+> ⟣ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿𝘀 & 𝗗𝗲𝘃: 𝐽𝑜𝑠𝑒 𝐶 - 𝐾𝑎𝑡ℎ𝑦 𝑍`
 
 function isOwnerByNumbers({ senderNum, senderNumDecoded }) {
   const owners = (config.owners || []).map(String)
