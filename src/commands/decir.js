3c3eb1a32b76ad29d354e0025f6f259e3bb50c44
Jose C <168625477+Jose-C21@users.noreply.github.com>
2026-07-23 22:58:24 -0600

    Actualizar decir.js

--- src/commands/decir.js
+++ src/commands/decir.js
@@ -6,7 +6,7 @@ import { execSync } from "child_process"
 import { getSenderJid, jidToNumber } from "../utils/jid.js"
 
 function signature() {
-  return `⟣ ©️ 𝓬𝓸𝓹𝔂𝓻𝓲𝓰𝓱𝓽|𝘑𝘊𝘒𝘡𝘐𝘯𝘧𝘪𝘯𝘪𝘵𝘺𝘓𝘰𝘰𝘱\n> ⟣ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿𝘀 & 𝗗𝗲𝘃: 𝐽𝑜𝑠𝑒 𝐶 - 𝐾𝑎𝑡ℎ𝑦`
+  return `⟣ ©️ 𝓬𝓸𝓹𝔂𝓻𝓲𝓰𝓱𝓽|𝘑𝘊𝘒𝘡𝘐𝘯𝘧𝘪𝘯𝘪𝘵𝘺𝘓𝘰𝘰𝘱\n> ⟣ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿𝘀 & 𝗗𝗲𝘃: 𝐽𝑜𝑠𝑒 𝐶 - 𝐾𝑎𝑡ℎ𝑦 𝑍`
 }
 
 // saca texto de quoted (si responden un mensaje)
